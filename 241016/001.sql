86332a57ebb0a10ad8442960bfdf6f2ef728f57b
Fierceplayers <61740086+forgetwangle@users.noreply.github.com>
2024-10-16 20:04:34 +0800

    Update and rename 001.txt to 001.sql

--- 241016/001.sql
+++ 241016/001.sql
@@ -1,3 +1,4 @@
+/*
 数据库，database,db,存放数据的仓库
 数据库管理系统，database management system,dbms，操纵管理db的大型软件
 sql,structed query language，操作关系型数据库的编程语言
@@ -6,3 +7,7 @@ sql,structed query language，操作关系型数据库的编程语言
 连接mysql，mysql -u root -p
 
 关系型数据库，rdbms，建立在关系模型的基础上，由多张相互连接的二维表组成的数据库
+
+导出sql文件
+mysqldump -u 用户名 -p 数据库名 > 备份文件名.sql
+*/
